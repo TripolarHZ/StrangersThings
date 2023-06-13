241a44bf194e025349814bdb956c11f89ae66eb2
TripolarHZ <hzha627@emory.edu>
2023-06-12 21:19:21 -0400

    new updates

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+export const COHORT_NAME = '2303-ftb-et-web-pt';
+export const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+export const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDg1MTdlMDBkYWI1ZjAwMTRmNDMwMGYiLCJ1c2VybmFtZSI6IlRyaXBvbGFySFoiLCJpYXQiOjE2ODY0OTI0MzR9.7rQXtP8Nd39yFogmbmpKB-rdA2PbTZB7mp5Ldfa_23w";
--- src/index.js
+++ src/index.js
@@ -14,11 +14,51 @@ import{
 
 const COHORT_NAME = '2303-ftb-et-web-pt';
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDg1MTdlMDBkYWI1ZjAwMTRmNDMwMGYiLCJ1c2VybmFtZSI6IlRyaXBvbGFySFoiLCJpYXQiOjE2ODY0OTI0MzR9.7rQXtP8Nd39yFogmbmpKB-rdA2PbTZB7mp5Ldfa_23w";
 
   const App = () =>{
 
     const [posts, setPosts] = useState([]);
     const [postId, setPostId] = useState(null);
+
+    const handleDelete = async (postIdToDelete) =>{
+      try {
+        const response = await fetch(`${BASE_URL}/posts/${postIdToDelete}`, {
+          method: "DELETE",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        const result = await response.json();
+        console.log(result);
+        return result
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    const postMessage = async (postIdToMessage) => {
+      try {
+        const response = await fetch(`${BASE_URL}/posts/${postIdToMessage}/messages`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify({
+            message: {
+              content: "Do you still have this?  Would you take $10 less?"
+            }
+          })
+        });
+        const result = await response.json();
+        console.log(result);
+        return result
+      } catch (err) {
+        console.error(err);
+      }
+    }
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -45,7 +85,8 @@ const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
                 <h2>Seller: {post.author.username}</h2>
                 <h4>Location: {post.location}</h4>
                 <button type="button" className="btn btn-outline-primary" onClick={()=>setPostId(post._id)} >Edit</button>
-                <button type="button" className="btn btn-outline-danger" >Delete</button>
+                <button type="button" className="btn btn-outline-danger" onClick={() => handleDelete(post._id)}>Delete</button>
+                <button type="button" className="btn btn-outline-primary" onClick={()=>postMessage(post._id)} >Message</button>
             </div>)
         }
     </div>
